dcf6cbb3581811b5abbc2c90be1390f7c9d7fab8
Pete Ryland <pete.ryland@magnolia-cms.com>
2016-04-12 16:20:46 +0200

    Example classes: Id, Maybe and Prob

--- js/workshop.js
+++ js/workshop.js
@@ -6,10 +6,6 @@ const constant = x => (y => x);
 const add  = ((x, y, z) => x + y + z).$;
 const mult = function(x, y) { return x * y; }.$;
 
-log("=============");
-log("== Reducer ==");
-log("=============");
-
 class Reducer {
   get unit()   { return undefined; } // abstract
   op(me, x, y) { return undefined; } // abstract
@@ -29,17 +25,73 @@ class Product extends Reducer {
 }
 const product = new Product();
 
-const mylist = [1, 2, 3, 4];
-
-const s = sum.rreduce(mylist);
-//log( s );
-
-const p = product.rreduce(mylist);
-//log( p );
-
 class Flatten extends Reducer {
   get unit()   { return []; }
   op(me, a, b) { return [...a, ...b]; }
 }
 const flatten = new Flatten();
-//log( flatten.rreduce([[1, 2],[3, 4]]) );
+
+
+log("--------------------");
+log("-- Identity Class --");
+log("--------------------");
+
+class _Id {
+  constructor(value) { this.value = value; }
+  toString() { return `Id(${this.value})`}
+}
+const Id = x => new _Id(x); // functional-style constructor
+
+const myid = Id(42); // example instance
+//log( myid );
+
+
+log("-----------------");
+log("-- Maybe Class --");
+log("-----------------");
+
+const [NOTHING, JUST] = [0, 1]
+class _Maybe {
+  constructor(tag, value) { this.tag = tag; this.value = value; }
+  toString() { return this.tag === NOTHING? "Nothing()" : `Just(${this.value})`}
+}
+const Nothing = () => new _Maybe(NOTHING); // functional-style constructors
+const Just = x => new _Maybe(JUST, x);
+
+const mymaybe = Just(7);
+//log( mymaybe );
+const mynothing = Nothing();
+//log( mynothing );
+
+class MaybeReducer extends Reducer {
+  get unit() { return Nothing; }
+  op(me, a, b) { return a.tag === JUST? a : b; }
+}
+const maybereducer = new MaybeReducer();
+
+//log( maybereducer.rreduce([Nothing(), Just(4), Just(5), Nothing()]) );
+
+class MaybeReducer2 extends Reducer {
+  constructor(parentreducer) { super(); this.parentop = parentreducer.op; }
+  get unit() { return Nothing(); }
+  op(me, a, b) { return a.tag === NOTHING? b : (b.tag === NOTHING? a : Just(me.parentop(me, a.value, b.value))); }
+}
+const maybesumreducer = new MaybeReducer2(sum);
+
+//log( maybesumreducer.rreduce([Nothing(), Just(4), Just(5), Nothing()]) );
+
+log("-----------------------");
+log("-- Probability Class --");
+log("-----------------------");
+
+class _Prob {
+  constructor(x) { this.x = x; } // expecting an array of {t: name, p: probability}
+  toString() { return `Prob([${this.x.map(y => "{t: " + y.t.toString() + ", p: " + y.p + "}\n     ")}])`; }
+}
+const Prob = x => new _Prob(x);
+
+const coin = Prob([{t: "Heads", p: 0.5}, {t: "Tails", p: 0.5}]);
+//log( coin );
+
+const loadedcoin = Prob([{t: "Heads", p: 0.6}, {t: "Tails", p: 0.4}]);
+//log( loadedcoin );
