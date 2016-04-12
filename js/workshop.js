f77d106b19ad38fc4194915903930b281dce41bd
Pete Ryland <pete.ryland@magnolia-cms.com>
2016-04-12 16:17:46 +0200

    Reducer (Monoid)

--- js/workshop.js
+++ js/workshop.js
@@ -1,42 +1,45 @@
 import './curry';
 const log = x => console.log(x.toString());
 
-log("=====================================");
-log("== FP-Style Functions and Currying ==");
-log("=====================================");
-
-// we need to get used to seeing functions defined using "fat arrow" or
-// "lambda" syntax
 const id = x => x;
 const constant = x => (y => x);
-
-// the `id` function
-//log( id(3) );
-
-// the `constant` function
-const myconstf = constant("hello");
-//log( myconstf(4) );
-
-// intro to currying
-// ignore the `.$` for now
-// these two are the same function, just with different ways to call
 const add  = ((x, y, z) => x + y + z).$;
-const add2 = x => (y => (z => x + y + z));
-
-// `add` gets called with three params
-//log( add(1, 2, 3) );
-
-// however, `add2` takes one param and returns a function that takes one param
-// and returns a function that takes one param
-//log( add2(1)(2)(4) );
-
-// this is where the `.$` comes in
-// it's from the curry import at the top and transforms any function into one
-// that can be (optionally) curried
-//log( add(1)(2)(5) );
-//log( add(1, 2)(6) );
-//log( add(1)(2, 7, 8) );
-
-// it also works when defining functions with the `function` keyword
 const mult = function(x, y) { return x * y; }.$;
-//log( mult(5)(4) );
+
+log("=============");
+log("== Reducer ==");
+log("=============");
+
+class Reducer {
+  get unit()   { return undefined; } // abstract
+  op(me, x, y) { return undefined; } // abstract
+
+  rreduce(x)   { return x.reduce(this.op.$(this), this.unit); }
+}
+
+class Sum extends Reducer {
+  get unit()   { return 0; }
+  op(me, a, b) { return a + b; }
+}
+const sum = new Sum();
+
+class Product extends Reducer {
+  get unit()   { return 1; }
+  op(me, a, b) { return a * b; }
+}
+const product = new Product();
+
+const mylist = [1, 2, 3, 4];
+
+const s = sum.rreduce(mylist);
+//log( s );
+
+const p = product.rreduce(mylist);
+//log( p );
+
+class Flatten extends Reducer {
+  get unit()   { return []; }
+  op(me, a, b) { return [...a, ...b]; }
+}
+const flatten = new Flatten();
+//log( flatten.rreduce([[1, 2],[3, 4]]) );
